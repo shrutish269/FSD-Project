b92df7987acf7e89b2d11491227fb5428ae9b659
Shruti Sharma <23bai70539>
2026-03-10 12:04:45 +0530

    Added labmst React API experiment

---
+++ labmst/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Fetch Users from API</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ labmst/src/components/Users.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+function Users() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+
+      <div className="user-list">
+        {users.map(user => (
+          <div key={user.id} className="user-card">
+            <h3>{user.name}</h3>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default Users;
